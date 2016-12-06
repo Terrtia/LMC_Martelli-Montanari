0c11ffd22325fc5f9b1d8d15a7cf737c14202560
nicolas <nicolas@nicolas-w250egq-w270egq-1.home>
2016-12-06 10:19:07 +0100

    commentaires + quelques fonctions terminés et testés

--- exo1.pl
+++ exo1.pl
@@ -1,17 +1,24 @@
-:- op(20,xfy,?=).
+:-
+	op(20,xfy,?=).
 
 % Prédicats d'affichage fournis
 
 % set_echo: ce prédicat active l'affichage par le prédicat echo
-set_echo :- assert(echo_on).
+set_echo :-
+	assert(echo_on).
 
 % clr_echo: ce prédicat inhibe l'affichage par le prédicat echo
-clr_echo :- retractall(echo_on).
+clr_echo :-
+	retractall(echo_on).
 
 % echo(T): si le flag echo_on est positionné, echo(T) affiche le terme T
 %          sinon, echo(T) réussit simplement en ne faisant rien.
 
-echo(T) :- echo_on, !, write(T).
+echo(T) :-
+	echo_on,
+	!,
+	write(T).
+
 echo(_).
 
 
@@ -22,21 +29,22 @@ regle(X?=T):-
 
 % Definitions des Transformations
 
-% Rename
+% Rename - finie
 rename(X?=T):-
 	var(T),
 	var(X),
 	X = T.
 
-% Simplify
+% Simplify - finie
 simplify(X?=T):-
 	atomic(T),
 	var(X),
 	X = T.
 
-% Orient
+% Orient - finie
 orient(T?=X):-
-	not(var(T)),
+	var(X),
+	nonvar(T),
 	Z = T,
 	T = X,
 	X = Z.
@@ -48,16 +56,34 @@ occur_check(V,T):-
 	var(V),
 	contains_var(V,T).
 
-% Check - en cours
+% Check - en cours - difficile
 check(X?=T):-
 	var(X),
-	not(X == T),
-	occur_check(X,T).
+	X == T,
+	not(occur_check(X,T)).
 
 % Expand - finie
 expand(X?=T):-
 	var(X),
 	not(atomic(T)),
-	not(var(T)),
+	nonvar(T),
 	occur_check(X,T),
 	X = T.
+
+% Decompose - en cours - difficile
+decompose(S?=T):-
+	compound(S),
+	compound(T),
+	functor(S,NameS,ArityS),
+	functor(T,NameT,ArityT),
+	NameS == NameT,
+	ArityS == ArityT,
+	arg(1,S,ValueS),
+	arg(1,T,ValueT),
+	ValueS ?= ValueT.
+
+% Clash - en cours
+clash(X?=T):-
+	var(X),
+	var(T),
+	X == T.
