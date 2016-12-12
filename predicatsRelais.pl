949dd1fc5b6403c54073b01bc48474541bf7a10a
nicolas <nicolas@nicolas-w250egq-w270egq-1.home>
2016-12-12 16:35:36 +0100

    soutenance

--- predicatsRelais.pl
+++ predicatsRelais.pl
@@ -3,22 +3,3 @@ splitEquation(E,X,T):-
 	arg(2,E,R),
 	X = L,
 	T = R.
-
-upto(Low,High,_Step,Low) :-
-	Low =< High.
-
-upto(Low,High,Step,Var) :-
-	Inc is Low+Step,
-	Inc =< High,
-	upto(Inc, High, Step, Var).
-
-%renommage(X,T,SystemEquation):-
-%	functor(SystemEquation,Name,Arity),
-%	Arity >= 1,
-%	SystemEquation = [Head | Tail],
-%	renommageHead(X,T,Head),
-%	renommage(X,T,Tail).
-%
-%renommageHead(X,T,Head):-
-%	
-
